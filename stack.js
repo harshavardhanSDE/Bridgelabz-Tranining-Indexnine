c53e1022fcc17ef136cb602bb7a52c29e0d6ac7a
harsha vardhan <108379858+harshavardhanSDE@users.noreply.github.com>
2025-11-13 12:51:39 +0530

    feat: stack using ll

--- stack.js
+++ stack.js
@@ -38,3 +38,44 @@ newStack.push(2);
 
 newStack.pop();
 newStack.peek();
+
+
+// stack using linked list
+
+class Node {
+    constructor(val) {
+        this.data = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    top = null;
+    constructor(){}
+
+    push(val) {
+        const temp = new Node(val);
+        // important, since every newly added node is the top node.
+        temp.next = this.top;
+        this.top = temp;
+        return this.top;
+    }
+
+    pop() {
+        if ( this.top == null) {console.log("stack is empty"); return -1};
+        let temp = this.top;
+        this.top = this.top.next;
+        let val = temp.data;
+
+        temp = null;
+
+        console.log(val);
+    }
+}
+
+let newstack = new Stack()
+newstack.push(10);
+newstack.push(23);
+newstack.pop()
+newstack.pop()
+newstack.pop()
